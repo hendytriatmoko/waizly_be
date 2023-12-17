5073cd7919048cc3d2f2c52d7081b8e39cc76c49
hendytriatmoko <hendytriatmoko0@gmail.com>
2023-12-17 14:23:05 +0700

    up

--- task_microservice/daos/task.go
+++ task_microservice/daos/task.go
@@ -58,7 +58,7 @@ func (m *Merk) TaskGet(params models.GetTask) ([]models.TaskGet, error) {
 
 	user := []models.TaskGet{}
 
-	err := databases.DatabaseWaizly.DB.Table("tasks")
+	err := databases.DatabaseWaizly.DB.Table("tasks").Select("*").Order("created_at desc")
 	if params.IdUser != "" {
 		err = err.Where("id_user = ?", params.IdUser)
 	}
@@ -74,6 +74,7 @@ func (m *Merk) TaskGet(params models.GetTask) ([]models.TaskGet, error) {
 	if params.Search != "" {
 		err = err.Where("nama ilike '%" + params.Search + "%' OR deskripsi ilike '%" + params.Search + "%'")
 	}
+
 	if params.Limit != nil {
 		err = err.Limit(*params.Limit)
 	}
